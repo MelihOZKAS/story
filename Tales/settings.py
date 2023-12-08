cea6993dfe97cc4e8029c1c9a9c81416ac9af659
MelihOZKAS <melihozkass@gmail.com>
2023-12-09 02:27:59 +0300

    ALLAH ım utandırma

--- Tales/settings.py
+++ Tales/settings.py
@@ -86,17 +86,17 @@ WSGI_APPLICATION = "Tales.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 #DATABASES = {
-#    "default": env.db()
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
 #}
+
+
+DATABASES = {
+    "default": env.db()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
