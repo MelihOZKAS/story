4326b9069688dab4ca91fd461094d96f427144d5
MelihOZKAS <melihozkass@gmail.com>
2025-02-04 04:17:41 +0300

    ALLAH ım utandırma

--- Tales/urls.py
+++ Tales/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import index, sitemap
 from Masallar.views import *
@@ -39,6 +39,7 @@ sitemaps = {
     'Kids-Friendship-Story': StoryFriendship,
 }
 
+
 def handler404(request, *args, **argv):
     response = render(request, '404.html')
     response.status_code = 404
@@ -46,13 +47,14 @@ def handler404(request, *args, **argv):
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("Masallar.urls")),
-    path('sitemap.xml/', index, {'sitemaps': sitemaps}),
-    path('sitemap-<section>.xml/', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path("robots.txt/",robots_txt, name="robots"),
-    path("Ads.txt/", ads, name="ads"),
-    path("ads.txt/", ads, name="ads"),
+                  path("admin/", admin.site.urls),
+                  path('sitemap.xml/', index, {'sitemaps': sitemaps}),
+                  path('sitemap-<section>.xml/', sitemap, {'sitemaps': sitemaps},
+                       name='django.contrib.sitemaps.views.sitemap'),
+                  path("robots.txt/", robots_txt, name="robots"),
+                  path("Ads.txt/", ads, name="ads"),
+                  path("ads.txt/", ads, name="ads"),
+                  path("", include("Masallar.urls")),
 
-
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                                                                                           document_root=settings.MEDIA_ROOT)
